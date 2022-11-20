3306a92535e49cdef2e6e66db91c1684b7c75534
Tigersmash333 <83250967+Tigersmash333@users.noreply.github.com>
2022-11-20 13:56:54 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from MusicBotSearch import Player
+import requests
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="?", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is ready.")
+    
+
+async def setup():
+    await bot.wait_until_ready()
+    bot.add_cog(Player(bot))
+
+bot.loop.create_task(setup())
+bot.run("OTIzMTI2MzYwNzI0MDgyNjg5.GZF_yf.37ETOcHVGWbqg0nSH6uwdoArmKYAKALYabCgpw")
